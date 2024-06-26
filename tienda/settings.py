83846565bd19536960e16ab41244512b89d9e48d
Benjv04 <benjamin.gonzalez040@gmail.com>
2024-06-26 18:11:52 -0400

    Configuraciones de settings

--- tienda/settings.py
+++ tienda/settings.py
@@ -118,9 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
--- waggypetshop/models.py
+++ waggypetshop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(primary_key=True, max_length=10)
+    contraseña = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre_usuario
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
