fb6a99f792f860b40f694c7e76b019d819cef7ef
Benjv04 <benjamin.gonzalez040@gmail.com>
2024-06-26 13:47:14 -0400

    Actualizacion urls.py, views.py e index

--- waggypetshop/urls.py
+++ waggypetshop/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name = 'index')
+    path('index', views.index, name = 'index'),
+    path('carrito', views.carrito, name = 'carrito'),
+    path('contacto', views.contacto, name = 'contacto'),
+    path('gatos', views.gatos, name = 'gatos'),
+    path('inicioSesion', views.inicioSesion, name = 'inicioSesion'),
+    path('nosotros', views.nosotros, name = 'nosotros'),
+    path('otros', views.otros, name = 'otros'),
+    path('perros', views.perros, name = 'perros'),
+    path('register', views.register, name = 'register'),
+    path('seguimiento', views.seguimiento, name = 'seguimiento'),
 ]
--- waggypetshop/views.py
+++ waggypetshop/views.py
@@ -4,3 +4,30 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request,'waggypetshop/index.html',context)
+
+def carrito(request):
+    return render(request, 'waggypetshop/carrito.html')
+
+def contacto(request):
+    return render(request, 'waggypetshop/Contacto.html')
+
+def gatos(request):
+    return render(request, 'waggypetshop/gatos.html')
+
+def inicioSesion(request):
+    return render(request, 'waggypetshop/inicioSesion.html')
+
+def nosotros(request):
+    return render(request, 'waggypetshop/Nosotros.html')
+
+def otros(request):
+    return render(request, 'waggypetshop/otros.html')
+
+def perros(request):
+    return render(request, 'waggypetshop/perros.html')
+
+def register(request):
+    return render(request, 'waggypetshop/register.html')
+
+def seguimiento(request):
+    return render(request, 'waggypetshop/seguimiento.html')
